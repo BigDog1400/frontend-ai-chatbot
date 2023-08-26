4453d6cac79871756659a90f3efdd2738b956f86
Isaac Rojo <const.isaac@gmail.com>
2023-08-26 23:49:58 +0000

    fix: update rq version

--- components/react-query.tsx
+++ components/react-query.tsx
@@ -2,14 +2,14 @@
 
 import React from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+// Create a client
+const queryClient = new QueryClient()
 
 function ReactQueryProvider({ children }: React.PropsWithChildren) {
-  const [client] = React.useState(new QueryClient());
 
   return (
-    <QueryClientProvider client={client}>
-      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+    <QueryClientProvider client={queryClient}>
+      {children}
     </QueryClientProvider>
   );
 }
