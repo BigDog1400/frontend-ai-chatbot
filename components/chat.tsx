afb85d0bcb6ed78b69277f34fd72af19db3c9f6c
Michael Chacon <mcljs15@gmail.com>
2023-08-26 15:29:29 -0400

    Add

--- components/chat.tsx
+++ components/chat.tsx
@@ -20,6 +20,9 @@ import { useState } from 'react'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 import { toast } from 'react-hot-toast'
+import { TextHint } from './text-hint'
+import clsx from 'clsx'
+import { XIcon } from './ui/icons'
 
 const IS_PREVIEW = process.env.VERCEL_ENV === 'preview'
 export interface ChatProps extends React.ComponentProps<'div'> {
@@ -33,6 +36,7 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
     null
   )
   const [previewTokenDialog, setPreviewTokenDialog] = useState(IS_PREVIEW)
+  const [ModalClose, setModalClose] = useState(false)
   const [previewTokenInput, setPreviewTokenInput] = useState(previewToken ?? '')
   const { messages, append, reload, stop, isLoading, input, setInput } =
     useChat({
@@ -48,17 +52,64 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
         }
       }
     })
+
+  const exampleMessages = [
+    {
+      heading: 'Explain technical concepts',
+      message: `What is a "serverless function"?`
+    },
+    {
+      heading: 'Summarize an article',
+      message: 'Summarize the following article for a 2nd grader: \n'
+    },
+    {
+      heading: 'Draft an email',
+      message: `Draft an email to my boss about the following: \n`
+    }
+  ]
   return (
     <>
-      <div className={cn('pb-[200px] pt-4 md:pt-10', className)}>
-        {messages.length ? (
-          <>
-            <ChatList messages={messages} />
-            <ChatScrollAnchor trackVisibility={isLoading} />
-          </>
-        ) : (
-          <EmptyScreen setInput={setInput} />
-        )}
+      <div className={cn('pb-[380px] pt-4 md:pt-10', className)}>
+        <ChatList messages={messages} ModalClose={ModalClose} setModalClose={setModalClose} />
+        <div
+          className={clsx('', {
+            'lg:mt-[510px] mt-[320px]': messages.length === 0
+          })}
+        >
+          <div
+            className={clsx('mx-auto max-w-2xl px-4 ', {
+              hidden: ModalClose
+            })}
+          >
+            <div className="rounded-lg border bg-background p-8 relative">
+              {messages.length > 0 && (
+                <button
+                  onClick={() => setModalClose(true)}
+                  type="button"
+                  className='absolute top-4 right-4 w-6 h-6 cursor-pointer'
+                >
+                  <XIcon />
+                </button>
+              )}
+
+              <h1 className="mb-2 text-lg font-semibold">Sugerencias:</h1>
+
+              <div className="mt-4 flex lg:flex-row flex-col items-start lg:space-x-2 lg:space-y-0 space-y-3">
+                {exampleMessages.map((message, index) => (
+                  <Button
+                    key={index}
+                    variant="secondary"
+                    className="h-auto py-1 px-3 text-base"
+                  >
+                    {message.heading}
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <ChatScrollAnchor trackVisibility={isLoading} />
       </div>
       <ChatPanel
         id={id}
