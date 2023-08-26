7ddb33008b14767c4eb085d4c9f1a3bff65d28af
chrisdadev13 <chrisdadev13@gmail.com>
2023-08-26 15:08:00 -0700

    Get the ticket list

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,4 +1,5 @@
 import { ThemeToggle } from '@/components/theme-toggle';
+import Link from 'next/link';
 
 const URL = process.env.API_URL;
 
@@ -6,30 +7,33 @@ export const Sidebar = async () => {
   const ticketList = await fetch(`${URL}/api/tickets`)
     .then(res => res.json())
     .catch(err => console.log(err));
+
+  console.log(ticketList);
+
   return (
-    <div className='w-72 flex flex-col justify-between inset-y-0 left-0 z-50 h-full border-r bg-background p-6 shadow-lg'>
+    <div className='w-72 flex flex-col inset-y-0 left-0 z-50 h-full border-r bg-background p-6 shadow-lg'>
       <div className='flex flex-col sm:flex-row sm:justify-start sm:space-x-2'>
         <h1>Tickets History</h1>
       </div>
       <div>
-        {ticketList.length ? (
-          <>
+        {ticketList ? (
+          <div className='flex flex-col'>
             {ticketList.map((ticket: any) => (
-              <div
-                className='relative max-h-5 my-3 flex-1 select-none overflow-hidden text-ellipsis break-all'
-                key={ticket.number}
-              >
-                <span className='whitespace-nowrap'>
+              <>
+                <Link
+                  href={`/${ticket.initialContext.toLowerCase().trim()}`}
+                  className='relative border p-3 rounded-md my-3 cursor-pointer'
+                >
                   {ticket.initialContext}
-                </span>
-              </div>
+                </Link>
+              </>
             ))}
-          </>
+          </div>
         ) : (
           <p>There are not tickets</p>
         )}
       </div>
-      <div>
+      <div className='flex-1'>
         <ThemeToggle />
       </div>
     </div>
