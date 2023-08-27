c8b14a604bd6098378363b13c3792da8018d44be
chrisdadev13 <chrisdadev13@gmail.com>
2023-08-26 18:18:37 -0700

    ticket

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,6 +1,8 @@
 'use client';
+
 import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
+import { usePathname, useParams } from 'next/navigation';
 
 export const Sidebar = () => {
   const { data } = useQuery({
@@ -12,6 +14,10 @@ export const Sidebar = () => {
     refetchInterval: 5000,
   });
 
+  const param = useParams();
+
+  console.log(param.id);
+
   return (
     <div className='w-72 left-0 z-10  border-r bg-background p-6 shadow-lg overflow-y-auto h-screen'>
       <div className='flex flex-col sm:flex-row sm:justify-start sm:space-x-2'>
@@ -20,12 +26,14 @@ export const Sidebar = () => {
       <div>
         {data ? (
           <div className='flex flex-col'>
-            {data.map((ticket: any) => (
+            {data.map((ticket: any, index: number) => (
               <>
                 <Link
                   href={`/chat/${ticket.number}`}
                   key={ticket.number}
-                  className={`relative rounded-lg border border-gray-300 shadow-sm p-3   my-3 cursor-pointer focus-within:ring-2 focus-within:ring-offset-2 hover:border-gray-400`}
+                  className={`relative rounded-lg border border-gray-300 shadow-sm p-3  ${
+                    parseInt(param.id) === index + 1 ? 'bg-red-400' : ''
+                  }  my-3 cursor-pointer focus-within:ring-2 focus-within:ring-offset-2 hover:border-gray-400`}
                 >
                   {ticket.initialContext}
                 </Link>
