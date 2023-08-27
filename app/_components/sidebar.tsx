4db63457f5388c8862ea0a2f1f74cf1207365ae1
chrisdadev13 <chrisdadev13@gmail.com>
2023-08-26 21:06:54 -0700

    hide sidebar on landing page and route to chat 1 afteer sign in

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -21,9 +21,9 @@ export const Sidebar = () => {
 
   if (path !== '/sign-in')
     return (
-      <div className='w-72 left-0 z-10  border-r bg-background p-6 shadow-lg overflow-y-auto h-screen'>
+      <div className='w-72 py-12 left-0 z-10  border-r bg-background p-6 shadow-lg overflow-y-auto h-screen'>
         <div className='flex flex-col sm:flex-row sm:justify-start sm:space-x-2'>
-          <h1>Tickets History</h1>
+          <h1 className='font-bold text-2xl'>Tickets History</h1>
         </div>
         <div>
           {data ? (
@@ -31,10 +31,10 @@ export const Sidebar = () => {
               {data.map((ticket: any, index: number) => (
                 <>
                   <Link
-                    href={`/chat/${ticket.number - 1}`}
-                    key={ticket.number}
+                    href={`/chat/${ticket.number}`}
+                    key={index}
                     className={`relative rounded-2xl border border-gray-300 shadow-sm p-3  ${
-                      parseInt(param.id) === index + 1 ? 'bg-gray-100' : ''
+                      parseInt(param.id) === ticket.number ? 'bg-gray-100' : ''
                     }  my-3 cursor-pointer focus-within:ring-2 focus-within:ring-offset-2 hover:border-gray-400`}
                   >
                     <p>Ticket {ticket.number - 1}</p>
