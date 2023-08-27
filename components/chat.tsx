f0fdf6f71ace9d8f7e4c4f406057f74cc523249d
chrisdadev13 <chrisdadev13@gmail.com>
2023-08-27 06:31:42 -0700

    Minor changes

--- components/chat.tsx
+++ components/chat.tsx
@@ -30,31 +30,23 @@ const IS_PREVIEW = process.env.VERCEL_ENV === 'preview';
 const URL = process.env.API_URL;
 
 export interface Root {
-  messages: Message[]
-  suggest: Suggest[]
+  messages: Message[];
+  suggest: Suggest[];
 }
 
-
 export interface Suggest {
-  options: string[]
-  suggests: string[]
+  options: string[];
+  suggests: string[];
 }
 
 export interface Message {
-  senderBy: 'client' | 'costumerService'
-  content: string
-  createAt: string
+  senderBy: 'client' | 'costumerService';
+  content: string;
+  createAt: string;
 }
-
-
-
-
-
-
 
 export interface ChatProps extends React.ComponentProps<'div'> {
   id?: string;
-
 }
 
 export function Chat({ id, className }: ChatProps) {
@@ -62,24 +54,24 @@ export function Chat({ id, className }: ChatProps) {
   const chatQuery = useQuery<Root>({
     queryKey: ['tickets-chat', id],
     queryFn: () =>
-      fetch(`https://backend-production-dbba.up.railway.app/api/tickets/${id}/chats`).then(
-        res => res.json(),
-      ),
+      fetch(
+        `https://backend-production-dbba.up.railway.app/api/tickets/${id}/chats`,
+      ).then(res => res.json()),
     refetchInterval: 5000,
   });
 
   const chatMessageMutation = useMutation({
-    mutationFn: (payload:{
-      senderBy: 'customerService',
-      content: string,
-    }) =>
-      fetch(`https://backend-production-dbba.up.railway.app/api/tickets/${id}/messages`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+    mutationFn: (payload: { senderBy: 'customerService'; content: string }) =>
+      fetch(
+        `https://backend-production-dbba.up.railway.app/api/tickets/${id}/messages`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...payload }),
         },
-        body: JSON.stringify({ ...payload }),
-      }),
+      ),
     onSuccess: () => {
       toast.success('Message sent!');
       queryClient.invalidateQueries();
@@ -87,12 +79,11 @@ export function Chat({ id, className }: ChatProps) {
     },
   });
 
-  console.log({ data: chatQuery.data })
+  console.log({ data: chatQuery.data });
   const [previewToken, setPreviewToken] = useLocalStorage<string | null>(
     'ai-token',
     null,
   );
-
 
   const [suggestMessage, setSuggestMessage] = useState('');
   const [previewTokenDialog, setPreviewTokenDialog] = useState(IS_PREVIEW);
@@ -103,23 +94,37 @@ export function Chat({ id, className }: ChatProps) {
   );
   const [input, setInput] = useState('');
 
-
-  if(chatQuery.isLoading) return <p>Loading...</p>
-  if(chatQuery.isError) return null
+  if (chatQuery.isLoading) return <p>Loading...</p>;
+  if (chatQuery.isError) return null;
 
   return (
     <>
       <aside className='inset-y-0 left-72 w-96 overflow-y-auto border-r border-gray-200 px-4 py-6 sm:px-6 lg:px-8 h-full'>
         <div className='col-span-1 flex gap-2 flex-col mx-auto mt-20'>
+          <div
+            className={`rounded-lg border  bg-gray-200 p-8 transition-all ease-in-out duration-200 shadow-smooth ${
+              true ? 'border-blue-50' : 'border-gray-300'
+            } cursor-pointer`}
+          >
+            <div className='font-bold text-2xl'>Cliente</div>
+          </div>
+          <div
+            className={`rounded-lg border bg-background p-8 transition-all ease-in-out duration-200 shadow-smooth ${
+              true ? 'border-gray-100' : 'border-gray-300'
+            } cursor-pointer`}
+          >
+            <div className='font-bold text-2xl'>Intermediario</div>
 
-             <div
-              className={`rounded-lg border bg-background p-8 transition-all ease-in-out duration-200 shadow-smooth ${true ? 'border-blue-400' : 'border-gray-300'} cursor-pointer`}
-            >
-              <div className='font-bold text-2xl'>
-                Jonh Doe
-              </div>
-            </div>
-          
+            <small>Disabled</small>
+          </div>
+          <div
+            className={`rounded-lg border bg-background p-8 transition-all ease-in-out duration-200 shadow-smooth ${
+              true ? 'border-gray-100' : 'border-gray-300'
+            } cursor-pointer`}
+          >
+            <div className='font-bold text-2xl'>Negocio</div>
+            <small>Disabled</small>
+          </div>
         </div>
       </aside>
 
@@ -155,31 +160,31 @@ export function Chat({ id, className }: ChatProps) {
                   <XIcon />
                 </button>
               )}
- <h1 className="mb-2 text-lg font-semibold">Sugerencias:</h1>
+              <h1 className='mb-2 text-lg font-semibold'>Sugerencias:</h1>
 
-<div className="mt-4 flex lg:flex-col flex-col items-start lg:space-x-0 lg:space-y-3 space-y-3">
-{
-  chatQuery.data?.suggest?.[0]?.options?.map((message, index) => (
-    <Button
-      key={'dd' +index}
-      variant="outline"
-      className="h-auto py-1 px-3 text-base"
-    >
-      {message}
-    </Button>
-  ))
-}
-   {chatQuery.data?.suggest[0]?.suggests?.map((message, index) => (
-    <Button
-      key={'dd' + index}
-      variant="secondary"
-      className="h-auto py-1 px-3 text-base"
-      onClick={e => setInput(message)}
-    >
-      {message}
-    </Button>
-  ))}
-</div>
+              <div className='mt-4 flex lg:flex-col flex-col items-start lg:space-x-0 lg:space-y-3 space-y-3'>
+                {chatQuery.data?.suggest?.[0]?.options?.map(
+                  (message, index) => (
+                    <Button
+                      key={'dd' + index}
+                      variant='outline'
+                      className='h-auto py-1 px-3 text-base'
+                    >
+                      {message}
+                    </Button>
+                  ),
+                )}
+                {chatQuery.data?.suggest[0]?.suggests?.map((message, index) => (
+                  <Button
+                    key={'dd' + index}
+                    variant='secondary'
+                    className='h-auto py-1 px-3 text-base'
+                    onClick={e => setInput(message)}
+                  >
+                    {message}
+                  </Button>
+                ))}
+              </div>
             </div>
           </div>
         </div>
@@ -207,7 +212,7 @@ export function Chat({ id, className }: ChatProps) {
       </Button>
       <ChatPanel
         isLoading={chatQuery.isLoading}
-        append={(message) => {
+        append={message => {
           chatMessageMutation.mutate({
             senderBy: 'customerService',
             content: message,
